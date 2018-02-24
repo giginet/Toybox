37f3fe7ee17847e5991d1b08d27fd8959bf8403a
giginet <giginet.net@gmail.com>
2018-02-24 15:47:54 +0900

    Update Formula

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -13,11 +13,11 @@ class Toybox < Formula
     sha256 "bcc9599a336a0043fb4eb0a48d2318b3db64ff111c7de8736d40b9deca3f37fa" => :sierra
   end
 
-  depends_on :xcode => ["8.0", :build]
+  depends_on :xcode
   depends_on 'carthage' => :build
 
   def install
-    system "make", "prefix_install", "PREFIX=#{prefix}"
+    system "make", "prefix_install", "PREFIX=#{HOMEBREW_PREFIX}"
     bash_completion.install "Sources/Scripts/toybox-bash-completion" => "toybox"
     zsh_completion.install "Sources/Scripts/toybox-zsh-completion" => "_toybox"
   end
