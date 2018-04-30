582aa826dfabb9e3df67c2cce8357a061ce377b7
giginet <giginet.net@gmail.com>
2018-04-30 17:17:40 +0900

    Update hash

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -2,7 +2,7 @@ class Toybox < Formula
   desc "Xcode Playground management made easy"
   homepage "https://github.com/giginet/Toybox"
   url "https://github.com/giginet/Toybox/archive/1.0.1.tar.gz"
-  sha256 "6ad5a80f06ae05e269fc581504a2ca539a93f0ebc207eafa21c1569c5f6e9390"
+  sha256 "315446308e40c33aa46fca1b2558d14e005d6e91505c50d4dc67cf87d38d7ee1"
   head "https://github.com/giginet/Toybox.git"
 
   depends_on :xcode
