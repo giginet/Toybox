2bbd17ed33a5bb847c0bd381ad954df63faab1aa
giginet <giginet.net@gmail.com>
2018-04-30 17:11:21 +0900

    Remove bottle

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -8,12 +8,6 @@ class Toybox < Formula
   depends_on :xcode
   depends_on 'carthage' => :build
 
-  bottle do
-    cellar :any
-    root_url "https://github.com/giginet/Toybox/releases/download/1.0.1"
-    sha256 "a835aa75cebdfa61cc73aabbe4d81a6192f347f8e2c0afc58497761caf12e3ac" => :high_sierra
-  end
-
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}"
     bash_completion.install "Sources/Scripts/toybox-bash-completion" => "toybox"
