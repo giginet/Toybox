825d482305bd9c04cca271f7c6934f2d7d4f51be
giginet <giginet.net@gmail.com>
2019-04-05 01:01:21 +0900

    Update brew

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -2,7 +2,7 @@ class Toybox < Formula
   desc "Xcode Playground management made easy"
   homepage "https://github.com/giginet/Toybox"
   url "https://github.com/giginet/Toybox/archive/2.0.0.tar.gz"
-  sha256 "946ecf32206ee40a0d1fc8846f440019cce157709c22586642a8ad6b08602a01"
+  sha256 "08ae74f3a4561b5ac6077ced9a613fac1a46c026b06f554599795d1940d96f55"
   head "https://github.com/giginet/Toybox.git"
 
   depends_on :xcode => ["10.2", :build]
@@ -10,7 +10,7 @@ class Toybox < Formula
   bottle do
     cellar :any_skip_relocation
     root_url "https://github.com/giginet/Toybox/releases/download/2.0.0"
-    sha256 "946ecf32206ee40a0d1fc8846f440019cce157709c22586642a8ad6b08602a01" => :high_sierra
+    sha256 "08ae74f3a4561b5ac6077ced9a613fac1a46c026b06f554599795d1940d96f55" => :high_sierra
   end
 
   def install
