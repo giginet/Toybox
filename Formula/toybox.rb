03ec6d16e59c816d5377eddad39147555fe451d2
giginet <giginet.net@gmail.com>
2019-04-13 14:59:02 +0900

    Update to 2.1.0

--- Formula/toybox.rb
+++ Formula/toybox.rb
@@ -1,8 +1,8 @@
 class Toybox < Formula
   desc "Xcode Playground management made easy"
   homepage "https://github.com/giginet/Toybox"
-  url "https://github.com/giginet/Toybox/archive/2.0.0.tar.gz"
-  sha256 " a2fa8907aacb8924ea672eb20eda5fff5c23f8b58eb75e90fd20774a0d4e0bac"
+  url "https://github.com/giginet/Toybox/archive/2.1.0.tar.gz"
+  sha256 "a2fa8907aacb8924ea672eb20eda5fff5c23f8b58eb75e90fd20774a0d4e0bac"
   head "https://github.com/giginet/Toybox.git"
 
   depends_on :xcode => ["10.2", :build]
