dcf090dde260944a324219813c260d3069fbcf93
giginet <giginet.net@gmail.com>
2019-06-11 07:10:31 +0900

    Add tests for multiple minor version

--- Tests/ToyboxKitTests/XcodeFinderTests.swift
+++ Tests/ToyboxKitTests/XcodeFinderTests.swift
@@ -2,13 +2,38 @@ import Foundation
 import XCTest
 @testable import ToyboxKit
 
+private func makeURL(from xcodeVersion: String) -> URL {
+    return URL(fileURLWithPath: "/Applications")
+        .appendingPathComponent("Xcode-\(xcodeVersion).app")
+}
+
+private func makePlistURL(from xcodeVersion: String) -> URL {
+    return makeURL(from: xcodeVersion)
+        .appendingPathComponent("Contents")
+        .appendingPathComponent("version.plist")
+}
+
+func makeData(from versionString: String) -> Data {
+    struct Version: Encodable {
+        let version: String
+        enum CodingKeys: String, CodingKey {
+            case version = "CFBundleShortVersionString"
+        }
+    }
+    
+    let encoder = PropertyListEncoder()
+    let version = Version(version: versionString)
+    return try! encoder.encode(version)
+}
+
 private class TestingXcodeFinder: XcodeFinder {
     typealias Loader = TestingDataLoader
     var xcodes: [URL] = []
     let dataLoader = TestingDataLoader()
     
     init(_ xcodes: [String]) {
-        self.xcodes = xcodes.map { URL(fileURLWithPath: "/Applications").appendingPathComponent("Xcode-\($0).app") }
+        self.xcodes = xcodes
+            .map(makeURL(from:))
     }
     
     func findXcodes() -> [URL] {
@@ -17,43 +42,37 @@ private class TestingXcodeFinder: XcodeFinder {
 }
 
 private class TestingDataLoader: DataLoader {
-    var data: Data?
+    private var dataMap: [URL: Data] = [:]
     
-    init() {
-        self.data = nil
+    func setVersions(_ versions: [String]) {
+        self.dataMap = versions.reduce(into: [:]) { $0[makePlistURL(from: $1)] = makeData(from: $1) }
     }
     
     func load(from path: URL) throws -> Data {
-        return data!
+        return dataMap[path]!
     }
-}
-
-func makeData(versionString: String) -> Data {
-    struct Version: Encodable {
-        let version: String
-        enum CodingKeys: String, CodingKey {
-            case version = "CFBundleShortVersionString"
-        }
-    }
-    
-    
-    let encoder = PropertyListEncoder()
-    let version = Version(version: versionString)
-    return try! encoder.encode(version)
 }
 
 final class XcodeFinderTests: XCTestCase {
     func testMajorVersion() {
         let finder = TestingXcodeFinder(["11"])
-        finder.dataLoader.data = makeData(versionString: "11.0")
+        finder.dataLoader.setVersions(["11"])
         let xcodePath = finder.find("11")
         XCTAssertEqual(xcodePath?.path, "/Applications/Xcode-11.app")
     }
     
     func testExactMatch() {
         let finder = TestingXcodeFinder(["10.2.1"])
-        finder.dataLoader.data = makeData(versionString: "10.2.1")
+        finder.dataLoader.setVersions(["10.2.1"])
         let xcodePath = finder.find("10.2.1")
+        XCTAssertEqual(xcodePath?.path, "/Applications/Xcode-10.2.1.app")
+    }
+    
+    func testMultipleMinors() {
+        let xcodes = ["10.2", "10.2.1", "10.0", "10.1"]
+        let finder = TestingXcodeFinder(xcodes)
+        finder.dataLoader.setVersions(xcodes)
+        let xcodePath = finder.find("10")
         XCTAssertEqual(xcodePath?.path, "/Applications/Xcode-10.2.1.app")
     }
 }
